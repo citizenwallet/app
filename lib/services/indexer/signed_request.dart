31cdf5b78f75312e2d6d7b907c49c7630e7aef56
kev-techi <kevin@techi.be>
2023-06-29 09:50:39 +0200

    [added] expiry to signature request

--- lib/services/indexer/signed_request.dart
+++ lib/services/indexer/signed_request.dart
@@ -4,6 +4,10 @@ import 'dart:typed_data';
 class SignedRequest {
   final Uint8List data;
   final String encoding = 'base64';
+  final int expiry = DateTime.now()
+      .add(const Duration(seconds: 5))
+      .toUtc()
+      .millisecondsSinceEpoch;
 
   SignedRequest(this.data);
 
@@ -11,5 +15,6 @@ class SignedRequest {
   Map<String, dynamic> toJson() => {
         'data': base64Encode(data),
         'encoding': encoding,
+        'expiry': expiry,
       };
 }
