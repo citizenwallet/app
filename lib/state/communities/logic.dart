5421933bc1169beae02d4f4e794a43bbb7f41407
sajee_ifonix <sajeewa@ifonix.io>
2024-09-15 18:32:39 +0530

    - load communities from db for community picker

--- lib/state/communities/logic.dart
+++ lib/state/communities/logic.dart
@@ -1,4 +1,6 @@
+import 'package:citizenwallet/services/config/config.dart';
 import 'package:citizenwallet/services/config/service.dart';
+import 'package:citizenwallet/services/db/app/db.dart';
 import 'package:citizenwallet/state/communities/state.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
@@ -6,24 +8,32 @@ import 'package:provider/provider.dart';
 class CommunitiesLogic {
   final CommunitiesState _state;
   final ConfigService config = ConfigService();
+  final AppDBService _db = AppDBService();
 
   CommunitiesLogic(BuildContext context)
       : _state = context.read<CommunitiesState>();
 
   Future<void> silentFetchCommunities() async {
     try {
-      final communities = await config.getConfigs();
+      _db.communities.refresh();
 
-      _state.upsertCommunities(communities);
+      final communities = await _db.communities.getAll();
+      List<Config> communityConfigs =
+          communities.map((c) => Config.fromJson(c.config)).toList();
 
-      for (final community in communities) {
-        if (community.community.hidden) {
+      _state.upsertCommunities(communityConfigs);
+
+      for (final communityConfig in communityConfigs) {
+        if (communityConfig.community.hidden) {
           continue;
         }
 
-        final isOnline = await config.isCommunityOnline(community.indexer.url);
+        final isOnline =
+            await config.isCommunityOnline(communityConfig.indexer.url);
+        await _db.communities
+            .updateOnlineStatus(communityConfig.community.alias, isOnline);
 
-        _state.setCommunityOnline(community.community.alias, isOnline);
+        _state.setCommunityOnline(communityConfig.community.alias, isOnline);
       }
 
       return;
@@ -36,9 +46,11 @@ class CommunitiesLogic {
     try {
       _state.fetchCommunitiesRequest();
 
-      final communities = await config.getConfigs();
+      final communities = await _db.communities.getAll();
+      List<Config> communityConfigs =
+          communities.map((c) => Config.fromJson(c.config)).toList();
 
-      _state.fetchCommunitiesSuccess(communities);
+      _state.fetchCommunitiesSuccess(communityConfigs);
       return;
     } catch (e) {
       //
