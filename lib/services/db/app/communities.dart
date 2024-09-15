1d5c7ef92af3079d15c0486e8c6e7758cff949be
sajee_ifonix <sajeewa@ifonix.io>
2024-09-15 18:29:37 +0530

    create community db table with some CRUD operations

--- lib/services/db/app/communities.dart
+++ lib/services/db/app/communities.dart
@@ -1,15 +1,240 @@
+import 'dart:convert';
+import 'package:citizenwallet/services/api/api.dart';
+import 'package:citizenwallet/services/config/service.dart';
+import 'package:citizenwallet/services/db/db.dart';
+import 'package:citizenwallet/utils/date.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:sqflite/sqflite.dart';
+
 class DBCommunity {
   final String alias; // index
-  final int version;
   final bool hidden;
-  final bool online;
-  final String config;
+  final Map<String, dynamic> config;
+  final int version;
+  bool online;
 
   DBCommunity({
     required this.alias,
-    required this.version,
-    required this.hidden,
     required this.config,
+    this.hidden = false,
+    this.version = 0,
     this.online = true,
   });
+
+  // process after reading from table
+  factory DBCommunity.fromMap(Map<String, dynamic> map) {
+    return DBCommunity(
+      alias: map['alias'],
+      config: jsonDecode(map['config']),
+      hidden: map['hidden'] == 1,
+      version: map['version'],
+      online: map['online'] == 1,
+    );
+  }
+
+  // process before inserting into table
+  Map<String, dynamic> toMap() {
+    return {
+      'alias': alias,
+      'version': version,
+      'hidden': hidden ? 1 : 0,
+      'online': online ? 1 : 0,
+      'config': jsonEncode(config),
+    };
+  }
+}
+
+class CommunityTable extends DBTable {
+  CommunityTable(super.db);
+
+  static const String communityConfigListLocalFileName =
+      kDebugMode ? 'communities.test' : 'communities';
+  static const String communityConfigListS3FileName = 'communities';
+
+  static const int version = 3;
+
+  final ConfigService _configService = ConfigService();
+
+  // The name of the table
+  @override
+  String get name => 't_community';
+
+  // The SQL query to create the table
+  @override
+  String get createQuery => '''
+    CREATE TABLE $name (
+      alias TEXT PRIMARY KEY,
+      hidden INTEGER NOT NULL DEFAULT 0,
+      config TEXT NOT NULL,
+      version INTEGER NOT NULL DEFAULT 0,
+      online INTEGER NOT NULL DEFAULT 1
+    )
+  ''';
+
+  // Creates the table and an index on the alias column
+  @override
+  Future<void> create(Database db) async {
+    await db.execute(createQuery);
+
+    // Create an index on the alias column
+    await db.execute('''
+      CREATE INDEX idx_${name}_alias ON $name (alias)
+    ''');
+
+    await seed();
+  }
+
+  // Migrates the table
+  @override
+  Future<void> migrate(Database db, int oldVersion, int newVersion) async {
+    final migrations = {};
+
+    for (var i = oldVersion + 1; i <= newVersion; i++) {
+      final queries = migrations[i];
+
+      if (queries != null) {
+        for (final query in queries) {
+          try {
+            await db.execute(query);
+          } catch (e, s) {
+            debugPrint('$name migration error, index $i: $e');
+            debugPrintStack(stackTrace: s);
+          }
+        }
+      }
+    }
+  }
+
+  Future<void> seed() async {
+    final String jsonString = await rootBundle.loadString(
+        'assets/config/v$version/$communityConfigListLocalFileName.json');
+
+    final List<dynamic> jsonList = jsonDecode(jsonString);
+
+    final List<Map<String, dynamic>> localConfigs =
+        jsonList.map((item) => Map<String, dynamic>.from(item)).toList();
+
+    // Check if the table is empty
+    final count =
+        Sqflite.firstIntValue(await db.rawQuery('SELECT COUNT(*) FROM $name'));
+    if (count != null && count > 0) {
+      return; // Table is not empty, skip seeding
+    }
+
+    // Prepare batch operation for efficient insertion
+    final batch = db.batch();
+
+    for (final data in localConfigs) {
+      final community = data['community'];
+
+      final alias = community['alias'];
+
+      final isHidden = community['hidden'] ?? false;
+      final hidden = isHidden ? 1 : 0;
+
+      final version = data['version'] ?? 0;
+
+      final indexer = data['indexer'];
+      final indexerUrl = indexer['url'];
+      final isOnline = await _configService.isCommunityOnline(indexerUrl);
+      final online = isOnline ? 1 : 0;
+
+      batch.insert(name, {
+        'alias': alias,
+        'hidden': hidden,
+        'config': jsonEncode(data),
+        'version': version,
+        'online': online,
+      });
+    }
+
+    await batch.commit(noResult: true);
+  }
+
+  void refresh() async {
+    final APIService api = APIService(baseURL: dotenv.get('WALLET_CONFIG_URL'));
+
+    final List<dynamic> response = await api.get(
+        url:
+            '/v$version/$communityConfigListS3FileName.json?cachebuster=${generateCacheBusterValue()}');
+
+    final List<Map<String, dynamic>> apiConfigs =
+        response.map((item) => Map<String, dynamic>.from(item)).toList();
+
+    // Prepare batch operation for efficient insertion
+    final batch = db.batch();
+
+    for (final data in apiConfigs) {
+      final community = data['community'];
+
+      final alias = community['alias'];
+
+      final isHidden = community['hidden'] ?? false;
+      final hidden = isHidden ? 1 : 0;
+
+      final version = data['version'] ?? 0;
+
+      final indexer = data['indexer'];
+      final indexerUrl = indexer['url'];
+      final isOnline = await _configService.isCommunityOnline(indexerUrl);
+      final online = isOnline ? 1 : 0;
+
+      batch.rawInsert('''
+        INSERT INTO $name (alias, hidden, config, version, online)
+        VALUES (?, ?, ?, ?, ?)
+        ON CONFLICT(alias) DO UPDATE SET
+          hidden = excluded.hidden,
+          config = excluded.config,
+          version = excluded.version,
+          online = excluded.online
+      ''', [alias, hidden, jsonEncode(data), version, online]);
+    }
+    await batch.commit(noResult: true);
+  }
+
+  Future<DBCommunity?> get(String alias) async {
+    final List<Map<String, dynamic>> maps = await db.query(
+      name,
+      where: 'alias = ?',
+      whereArgs: [alias],
+      limit: 1,
+    );
+
+    if (maps.isEmpty) {
+      return null;
+    }
+
+    return DBCommunity.fromMap(maps.first);
+  }
+
+  Future<List<DBCommunity>> getAll() async {
+    final List<Map<String, dynamic>> maps = await db.query(name);
+
+    return List.generate(maps.length, (i) {
+      return DBCommunity.fromMap(maps[i]);
+    });
+  }
+
+  Future<List<DBCommunity>> getOnline() async {
+    final List<Map<String, dynamic>> maps = await db.query(
+      name,
+      where: 'online = ?',
+      whereArgs: [1],
+    );
+
+    return List.generate(maps.length, (i) {
+      return DBCommunity.fromMap(maps[i]);
+    });
+  }
+
+  Future<void> updateOnlineStatus(String alias, bool online) async {
+    await db.update(
+      name,
+      {'online': online ? 1 : 0},
+      where: 'alias = ?',
+      whereArgs: [alias],
+    );
+  }
 }
