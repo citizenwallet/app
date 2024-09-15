b84a9523eff86f4027824c5b915ec1cb412ffb3a
sajee_ifonix <sajeewa@ifonix.io>
2024-09-15 18:30:29 +0530

    extend db service abstract with App level db service

---
+++ lib/services/db/app/db.dart
@@ -0,0 +1,40 @@
+import 'package:citizenwallet/services/db/app/communities.dart';
+import 'package:citizenwallet/services/db/db.dart';
+import 'package:sqflite/sqflite.dart';
+
+class AppDBService extends AbsDBService {
+  static final AppDBService _instance = AppDBService._internal();
+
+  factory AppDBService() {
+    return _instance;
+  }
+
+  AppDBService._internal();
+
+  late CommunityTable communities;
+
+  @override
+  Future<Database> openDB(String path) async {
+    final options = OpenDatabaseOptions(
+      onConfigure: (db) async {
+        communities = CommunityTable(db);
+      },
+      onCreate: (db, version) async {
+        await communities.create(db);
+        return;
+      },
+      onUpgrade: (db, oldVersion, newVersion) async {
+        await communities.migrate(db, oldVersion, newVersion);
+        return;
+      },
+      version: 1,
+    );
+
+    final db = await databaseFactory.openDatabase(
+      path,
+      options: options,
+    );
+
+    return db;
+  }
+}
