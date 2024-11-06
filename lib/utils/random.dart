1e41b7abd4172c714c4c4aae06b313b043429a3e
sajee_techi <sajeewa@techi.be>
2024-11-06 12:49:07 +0530

    flutter package to generator random nouns

--- lib/utils/random.dart
+++ lib/utils/random.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:math';
+import 'package:word_generator/word_generator.dart';
 
 String getRandomString(int len) {
   final random = Random.secure();
@@ -21,4 +22,16 @@ int getRandomNumber({int len = 6}) {
   final values = List<int>.generate(len, (i) => random.nextInt(9));
 
   return int.parse(values.join());
+}
+
+String getRandomNoun() {
+  final wordGenerator = WordGenerator();
+  return wordGenerator.randomNoun();
+}
+
+String getRandomUsername() {
+  final number = getRandomNumber(len: 4);
+  final noun = getRandomNoun();
+
+  return '$noun-$number';
 }
