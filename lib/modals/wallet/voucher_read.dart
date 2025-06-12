9c04c7f2cacd36752c9cbb181bb748f6d84c1d76
Viran Ranaraja <viranranaraja@192.168.1.6>
2025-06-12 14:13:45 +0530

    Localization Update

--- lib/modals/wallet/voucher_read.dart
+++ lib/modals/wallet/voucher_read.dart
@@ -15,7 +15,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:citizenwallet/l10n/app_localizations.dart';
 
 class VoucherReadModal extends StatefulWidget {
   final String address;
--- lib/screens/wallet/screen.dart
+++ lib/screens/wallet/screen.dart
@@ -27,7 +27,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:citizenwallet/l10n/app_localizations.dart';
 import 'package:citizenwallet/widgets/communities/offline_banner.dart';
 import 'package:citizenwallet/widgets/webview/connected_webview_modal.dart';
 
