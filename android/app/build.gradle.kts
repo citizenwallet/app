2e03186942d214189d24b9e9a4218190f168601a
kev-techi <kevin@techi.be>
2025-07-05 14:54:12 +0200

    remove duplicate kotlin file

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "xyz.citizenwallet.citizenwallet"
+    namespace = "xyz.citizenwallet.wallet"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "xyz.citizenwallet.citizenwallet"
+        applicationId = "xyz.citizenwallet.wallet"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/xyz/citizenwallet/citizenwallet/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package xyz.citizenwallet.citizenwallet
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
